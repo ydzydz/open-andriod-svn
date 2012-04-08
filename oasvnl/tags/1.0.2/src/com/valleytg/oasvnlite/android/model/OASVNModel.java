8d6b91015e01289917091f72c95b56ffa66fd943
brian <brian@25f97090-fc74-1751-5737-5d7e53ec7b91>
2012-04-08 01:12:48 +0000

    OASVN release 1.0.2

---
+++ oasvnl/tags/1.0.2/src/com/valleytg/oasvnlite/android/model/OASVNModel.java
@@ -0,0 +1,61 @@
+package com.valleytg.oasvnlite.android.model;
+
+import java.util.Date;
+
+import com.valleytg.oasvnlite.android.util.DateUtil;
+
+public abstract class OASVNModel {
+	
+	/**
+	 * Last modified date.  Updated with any change to record
+	 */
+	private Date dateModified;
+	
+	/**
+	 * Date record was initially created.
+	 */
+	private Date dateCreated;
+	
+	/**
+	 * Active flag
+	 */
+	private Boolean active; 
+	
+	/**
+	 * Constructor
+	 */
+	public OASVNModel() {
+		// set the create time 
+		this.setDateCreated(DateUtil.getGMTNow());
+		this.setDateModified(DateUtil.getGMTNow());
+
+	}
+
+	// gettors and settors
+
+	public Date getDateModified() {
+		return this.dateModified;
+	}
+
+	public void setDateModified(Date dateModified) {
+		this.dateModified = dateModified;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	
+}
---
+++ oasvnl/tags/1.0.2/src/com/valleytg/oasvnlite/android/ui/activity/AddRepository.java
@@ -0,0 +1,97 @@
+package com.valleytg.oasvnlite.android.ui.activity;
+
+import com.valleytg.oasvnlite.android.R;
+import com.valleytg.oasvnlite.android.application.OASVNApplication;
+import com.valleytg.oasvnlite.android.model.Connection;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+public class AddRepository extends Activity {
+	
+	/**
+	 * Application
+	 */
+	OASVNApplication app;
+
+	Button btnSave;
+	Button btnBack;
+	EditText name;
+	EditText url;
+	EditText username;
+	EditText password;
+	EditText folder;
+	
+	/**
+	 * working connection
+	 */
+	Connection thisConnection;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.addrepos);
+	
+		// get the application
+        this.app = (OASVNApplication)getApplication();
+		
+		btnSave = (Button) findViewById(R.id.add_save);
+        btnBack = (Button) findViewById(R.id.add_cancel);
+        
+        name = (EditText) findViewById(R.id.add_name);
+		url = (EditText) findViewById(R.id.add_url);
+		username = (EditText) findViewById(R.id.add_username);
+		password = (EditText) findViewById(R.id.add_password);
+		folder = (EditText) findViewById(R.id.add_folder);
+        
+        // check to see if we are editing a cannection
+        if(app.getCurrentConnection() != null) {
+        	thisConnection = app.getCurrentConnection();
+        	
+        	name.setText(app.getCurrentConnection().getName());
+        	url.setText(app.getCurrentConnection().getTextURL());
+        	username.setText(app.getCurrentConnection().getUsername());
+        	password.setText(app.getCurrentConnection().getPassword());
+        	folder.setText(app.getCurrentConnection().getFolder());
+        }
+        else {
+	        // initialize the connection
+	        thisConnection = new Connection();
+        }
+        
+        // button listeners     
+        this.btnBack.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				AddRepository.this.finish();
+			}
+		});
+        
+        this.btnSave.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				// do the save
+				AddRepository.this.save();
+				AddRepository.this.finish();
+			}
+		});
+	}
+
+	private void save() {
+		
+		thisConnection.setName(name.getText().toString());
+		thisConnection.setUrl(url.getText().toString());
+		thisConnection.setUsername(username.getText().toString());
+		thisConnection.setPassword(password.getText().toString());
+		thisConnection.setFolder(folder.getText().toString());
+		
+		thisConnection.saveToLocalDB(this.app);
+	}
+	
+}
